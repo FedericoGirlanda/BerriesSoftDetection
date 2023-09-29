8c59b84be930ffdbb0bc58c107044dfc2dbb1aec
FedericoGirlanda <fedegirla.fg@gmail.com>
2023-09-29 17:53:14 +0200

    Adding python code

---
+++ src/python/softDetection.py
@@ -0,0 +1,123 @@
+from keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+import cv2
+
+from utils import plot_one_box, filterBox, getBoxes, nonMaxSuppress, centroid, boxesCenters, lowpass
+
+# Load the model
+model = load_model('models/YOLOv5_5')
+dim = (640,640) #(320, 320) # input image dimension
+
+#####################
+# Detection on Image
+#####################
+
+# Load the image
+img_path = "dataset/testing/IMG_1071.jpg.4b38bvv8.ingestion-55b478f466-cp67z.jpg"
+img = load_img(img_path, target_size=dim)
+
+# Convert the image to a numpy array
+input_data = img_to_array(img)
+
+# Normalize the pixel values
+input_data /= 255.0
+
+# Add a dimension to match the model's input shape
+input_data = np.expand_dims(input_data, axis=0)
+
+# Make a prediction
+prediction = model.predict(input_data)[0] #(x,  y,  w,  h,  confidence,  class_n) = prediction.T
+prediction = filterBox(prediction,0.3)
+
+# Convert to Boxes
+boxes = getBoxes(prediction, dim)
+
+# NMS
+boxes = nonMaxSuppress(boxes,0.8)
+print(f"Coarse number of berries: ", len(boxes))
+
+# Calculate the centroid of the boxes
+centers = boxesCenters(boxes)
+c = centroid(centers)
+print(f"Centroid: ", c)
+
+# Load the input image (in OpenCV format), resize it and plot the bounding boxes
+image = cv2.imread(img_path)
+image = cv2.resize(image, dim, interpolation = cv2.INTER_NEAREST)
+#image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+for i in range(len(boxes)):
+    plot_one_box(boxes[i], image, (0,0,255), "berry", 1)
+#image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+image = cv2.resize(image, dim, interpolation = cv2.INTER_NEAREST)
+cv2.arrowedLine(image, (int(dim[0]/2), int(dim[1]/2)), (int(c[0]), int(c[1])), (255,255,255), 3, 5, 0, 0.1)
+
+# Show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
+
+#####################
+# Detection on Video
+#####################
+
+# # initialize the video stream, pointer to output video file, and
+# # frame dimensions
+# vs = cv2.VideoCapture("dataset/IMG_1119.MOV")
+# writer = None
+# (W, H) = (None, None)
+# c0_prev = 0
+# c1_prev = 0
+# beta = 0.8
+# # loop over frames from the video file stream
+# while True:
+# 	# read the next frame from the file
+# 	(grabbed, frame) = vs.read()
+# 	# if the frame was not grabbed, then we have reached the end of the stream
+# 	if not grabbed:
+# 		break
+# 	# if the frame dimensions are empty, grab them
+# 	if W is None or H is None:
+# 		(H, W) = frame.shape[:2]
+		
+#     # clone the output frame, then preprocess it
+# 	output = frame.copy()
+# 	frame = cv2.resize(frame, dim).astype("float32")
+# 	input_data = img_to_array(frame)
+# 	input_data /= 255.0
+
+#     # make predictions on the frame 
+# 	prediction = model.predict(np.expand_dims(frame, axis=0))[0]
+# 	prediction = filterBox(prediction,0.3)
+
+# 	if len(prediction) > 1:
+# 		boxes = getBoxes(prediction, dim)
+# 		boxes = nonMaxSuppress(boxes,0.8)
+# 		if len(boxes)> 1:
+# 			centers = boxesCenters(boxes)
+# 			c = centroid(centers)
+# 			# draw on the output frame
+# 			for i in range(len(boxes)):
+# 				plot_one_box(boxes[i], frame, (0,0,255), "berry", 1)
+# 			c0_prev = lowpass(c0_prev,c[0],beta)
+# 			c1_prev = lowpass(c1_prev,c[1],beta)
+# 			cv2.arrowedLine(frame, (int(dim[0]/2), int(dim[1]/2)), (int(c0_prev), int(c1_prev)), (255,255,255), 3, 5, 0, 0.1)
+
+# 	# # check if the video writer is None
+# 	# if writer is None:
+# 	# 	# initialize our video writer
+# 	# 	fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+# 	# 	writer = cv2.VideoWriter(args["output"], fourcc, 30,
+# 	# 		(W, H), True)
+# 	# # write the output frame to disk
+# 	# writer.write(output)
+	
+# 	# show the output image
+# 	cv2.imshow("Output", frame)#output)
+# 	key = cv2.waitKey(1) & 0xFF
+# 	# if the `q` key was pressed, break from the loop
+# 	if key == ord("q"):
+# 		break
+# # release the file pointers
+# print("[INFO] cleaning up...")
+# writer.release()
+# vs.release()
---
+++ src/python/utils.py
@@ -0,0 +1,121 @@
+import random
+import cv2
+import numpy as np
+
+"""
+Plot bounding boxes from a prediction on the image img
+"""
+
+def plot_one_box(boxes, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = ((int(boxes[0]),int(boxes[1])),(int(boxes[2]),int(boxes[3])))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+"""
+Get bounding box locations from raw output
+"""
+
+def getBoxes(predictions, dimension):
+    boxes = np.array([int((predictions[0][0] - (0.5 * predictions[0][2]))*dimension[0]), 
+                      int((predictions[0][1] - (0.5 * predictions[0][3]))*dimension[1]), 
+                      int((predictions[0][0] + (0.5 * predictions[0][2]))*dimension[0]), 
+                      int((predictions[0][1] + (0.5 * predictions[0][3]))*dimension[1]),
+                      predictions[0][4]])
+    for i in range(1,len(predictions)):
+        boxes = np.vstack((boxes,[int((predictions[i][0] - (0.5 * predictions[i][2]))*dimension[0]), 
+                                  int((predictions[i][1] - (0.5 * predictions[i][3]))*dimension[1]), 
+                                  int((predictions[i][0] + (0.5 * predictions[i][2]))*dimension[0]), 
+                                  int((predictions[i][1] + (0.5 * predictions[i][3]))*dimension[1]),
+                                  predictions[i][4]]))
+    return boxes
+
+"""
+Scale bounding box location for representation
+"""
+
+def scaleBox(p1,p2,w,h):
+    return (int(p1[0]*w), int(p1[1]*h)), (int(p2[0]*w), int(p2[1]*h))
+
+"""
+Filter out boxes with low confidence 
+"""
+
+def filterBox(predictions, scoreThresh=0.5):
+    return predictions[np.where(predictions.T[4]>scoreThresh)[0]]
+
+"""
+Filter out boxes that are significantly overlapped. Only preserve the box with the highest score.
+This function should be called after filterBox to reduce workload
+"""
+
+def nonMaxSuppress(boxes, iouThresh=0.7):
+    keep_idx = []
+    sorted_indeces = np.array(boxes).T[4].argsort()
+    boxes = np.array(boxes)[sorted_indeces]
+    for i in range(len(boxes)):
+        to_keep = True
+        for j in range(i,len(boxes)):
+            if box_iou(boxes[i], boxes[j]) > iouThresh and boxes[i][4] < boxes[j][4]:
+                    to_keep = False
+        if to_keep:
+            keep_idx = np.append(keep_idx,i)
+    return [boxes[int(k)] for k in keep_idx]
+
+"""
+Function that returns the IoU between two boxes
+"""
+def box_iou(box_a, box_b):
+
+    def box_area(box):
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area_a = box_area(box_a)
+    area_b = box_area(box_b)
+
+    x_left = np.maximum(box_a[0], box_b[0])
+    y_left = np.maximum(box_a[1], box_b[1])
+    x_right = np.minimum(box_a[2], box_b[2])
+    y_right = np.minimum(box_a[3], box_b[3])
+    area_inter = np.clip(box_area([x_left,y_left,x_right,y_right]), a_min=0, a_max = np.minimum(area_a,area_b))
+    area_union = np.clip(area_a + area_b - area_inter, a_min=np.maximum(area_a,area_b), a_max = (area_a+area_b))
+    if box_a[2] < box_b[0]:
+        area_inter = 0
+    return area_inter / area_union
+
+"""
+Function that calculate the centrid of a set of points
+"""
+def centroid(points):
+    sum_x = 0
+    sum_y = 0
+    for i in range(len(points)):
+        sum_x += points[i][0]
+        sum_y += points[i][1]
+    return (int(sum_x/len(points)), int(sum_y/len(points)))
+
+"""
+Function that calculate centers of boxes
+"""
+def boxesCenters(boxes):
+    w0 = boxes[0][2] - boxes[0][0] 
+    h0 = boxes[0][3] - boxes[0][1]
+    centers = np.array([boxes[0][0]+w0/2,boxes[0][1]+h0/2])
+    for i in range(1,len(boxes)):
+        w = boxes[i][2] - boxes[i][0] 
+        h = boxes[i][3] - boxes[i][1] 
+        centers = np.vstack((centers, np.array([boxes[i][0]+w/2, boxes[i][1]+h/2])))
+    return centers
+
+"""
+Function that implements a lowpass filter
+"""
+def lowpass(prev, next, beta):
+    return beta*prev + (1-beta)*next
